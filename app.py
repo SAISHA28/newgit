32799f5b49d41e2852db3c6d919b3193a338b733
SJ <saisha,j28@gmail.com>
2026-04-22 17:36:11 +0530

    Updated JSON file in saigit_new branch

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello Tutedude!"
+@app.route('/api')
+def api():
+    with open('data.json') as f:
+        data = json.load(f)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
